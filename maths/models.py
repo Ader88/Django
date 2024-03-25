4a842e37380ba6506fcbc2c1f23c5bce09edb1be
Adrian Potoniec <adrian.potoniec@gmail.com>
2024-03-25 18:35:59 +0100

    Update files

--- maths/models.py
+++ maths/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Math(models.Model):
+    operation = models.CharField(max_length=5)
+    a = models.IntegerField()
+    b = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        app_label = 'maths'
+
+class Result(models.Model):  # Dodaj definicję klasy Result
+    # Definicja pól dla klasy Result
+    pass
--- maths/tests.py
+++ maths/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from maths.models import Math
 
-# Create your tests here.
+class MathModelTest(TestCase):
+
+    def setUp(self):
+        Math.objects.create(operation="add", a=1, b=2)
+        Math.objects.create(operation="sub", a=20, b=30)
+
+    def test_math_str(self):
+        m1 = Math.objects.get(operation="add")
+        m2 = Math.objects.get(operation="sub")
+
+        self.assertEqual(str(m1), "Math object (1)")
+        self.assertEqual(str(m2), "Math object (2)")
