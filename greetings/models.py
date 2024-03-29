2aed92963e1027ffaa2c06f7fa4d2ccb4895e491
Adrian Potoniec <adrian.potoniec@gmail.com>
2024-03-29 20:45:05 +0100

    Update files

--- greetings/models.py
+++ greetings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class GreetingCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Greeting(models.Model):
+    category = models.ForeignKey(GreetingCategory, on_delete=models.CASCADE)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.message
