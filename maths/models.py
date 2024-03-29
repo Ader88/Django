87b552c855c1acc103d707059fbcaa8054ad88b7
Adrian Potoniec <adrian.potoniec@gmail.com>
2024-03-29 13:22:18 +0100

    Update files

--- maths/models.py
+++ maths/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 
-# Create your models here.
-
 class Math(models.Model):
     a = models.IntegerField()
     b = models.IntegerField()
     operation = models.CharField(max_length=10)
     created = models.DateTimeField(auto_now_add=True)
+    result = models.IntegerField(null=True, default=None)
 
     def __str__(self):
-        return f"id:{self.id}, a={self.a}, b={self.b}, op={self.operation}"
+        return f"id:{self.id}, a={self.a}, b={self.b}, op={self.operation}, {self.result}"
     
 class Result(models.Model):
-    value = models.FloatField()
+    value = models.FloatField(default=0.0)  # Domyślna wartość dla value to 0.0
     error = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
